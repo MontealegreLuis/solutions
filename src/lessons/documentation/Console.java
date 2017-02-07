51f83cb535f30431d687b4a8e2f816189719be61
Luis Montealegre <montealegreluis@gmail.com>
2017-02-06 21:24:39 -0600

    Create skeleton for Console class.

---
+++ src/lessons/documentation/Console.java
@@ -0,0 +1,39 @@
+/**
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+package lessons.documentation;
+
+public class Console {
+    public void print(String s) {
+    }
+
+    public void println(String s) {
+    }
+
+    public void println() {
+    }
+
+    public String getRequiredString(String prompt) {
+        return null;
+    }
+
+    public String getChoiceString(String prompt, String s1, String s2) {
+        return null;
+    }
+
+    public int getInt(String prompt) {
+        return 0;
+    }
+
+    public int getIntWithinRange(String prompt, int min, int max) {
+        return 0;
+    }
+
+    public double getDouble(String prompt) {
+        return 0.0;
+    }
+
+    public double getDoubleWithinRange(String prompt, double min, double max) {
+        return 0.0;
+    }
+}
